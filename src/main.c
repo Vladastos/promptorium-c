3e950bce18eab6da92d652e6cafcf500ac650708
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2024-07-12 11:39:53 +0200

    First commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+
+    //get the current working directory
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf("Current working dir: %s\n", cwd);
+
+    //get the user name
+    char *user = getlogin();
+    printf("User name: %s\n", user);
+    printf("Hello, World!\n");
+    return 0;
+}
